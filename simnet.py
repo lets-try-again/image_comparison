50a4fd8b3c47f7eaca184b9ea16a617b448b09d8
ourvakan <dpiliposyan@fundseeder.com>
2020-04-21 11:56:42 +0400

    refactoring

--- simnet.py
+++ simnet.py
@@ -9,43 +9,38 @@ import tensorflow as tf
 tf.config.experimental.set_visible_devices([], 'GPU')
 from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from tensorflow.keras.utils import plot_model
 
 from preprocessing.scaler import shrink
 from preprocessing.loader import load_and_split
 from preprocessing.dimreduct import reduce_dim
 from utils.plot_utils import plot_loss, plot_embedding, plot_pair
 
+
 np.set_printoptions(suppress=True)
 
 
 class Encoder(Model):
     """
-    A network that finds a 10-dimensional representation of the input images
-    so that the distances between them minimize the SimNet loss
+    A network that finds a 50-dimensional representation of the input images
+    so that the distances between them minimize the constructive loss
     """
 
     def __init__(self):
-        super().__init__()
+        super(Encoder, self).__init__(name='encoder')
 
-        self.cv = Conv2D(24, (3, 3), activation='relu', padding='Same',
+        self.cv = Conv2D(32, (3, 3), activation='relu', padding='Same',
                          input_shape=(28, 28, 1), kernel_initializer=tf.keras.initializers.glorot_normal(),
                          kernel_regularizer=tf.keras.regularizers.l2(0.01))
         self.pool = MaxPooling2D((2, 2))
-        self.cv2 = Conv2D(24, (3, 3), activation='relu', padding='Same',
-                          input_shape=(28, 28, 1), kernel_initializer=tf.keras.initializers.glorot_normal(),
-                          kernel_regularizer=tf.keras.regularizers.l2(0.01))
-        self.pool2 = MaxPooling2D((2, 2))
         self.flatten = Flatten()
-        self.dense = Dense(5, activation=None, kernel_initializer=tf.keras.initializers.glorot_normal(),
+        self.dense = Dense(50, activation=None, kernel_initializer=tf.keras.initializers.glorot_normal(),
                            kernel_regularizer=tf.keras.regularizers.l2(0.01))
 
-    @tf.function
-    def call(self, x):
+    def call(self, inputs, training=None, mask=None):
         """ Forward pass for one image """
-        x = self.cv(x)
+        x = self.cv(inputs)
         x = self.pool(x)
-        x = self.cv2(x)
-        x = self.pool2(x)
         x = self.flatten(x)
         x = self.dense(x)
         return x
@@ -53,9 +48,7 @@ class Encoder(Model):
     @staticmethod
     def distance(difference):
         """ The D function from the paper which is used in loss """
-        # return tf.nn.tanh(tf.reduce_mean(tf.sqrt(tf.square(difference))))
         distance = tf.sqrt(tf.reduce_sum(tf.pow(difference, 2), 0))
-        # return tf.nn.tanh(tf.norm(difference))
         return distance
 
     def make_predict(self, pair, threshold=0.5):
@@ -65,22 +58,19 @@ class Encoder(Model):
         x1, x2 = pair[:, 0, :, :, :], pair[:, 1, :, :, :]
         x1 = self.call(x1)
         x2 = self.call(x2)
-        out = x2 - x1
-        distance_vector = tf.map_fn(lambda x: Encoder.distance(x), out)
+        difference_vector = x2 - x1
+        distance_vector = tf.map_fn(lambda x: Encoder.distance(x), difference_vector)
         # apply threshold
         distance_vector = tf.map_fn(lambda x: 0.0 if x <= threshold else 1.0, distance_vector)
         return distance_vector
 
 
-@tf.function
 def custom_accuracy(y_true, y_pred):
     distance_vector = tf.map_fn(lambda x: Encoder.distance(x), y_pred)
-    # distance_vector = tf.map_fn(lambda x: 0.0 if x <= 0.5 else 1.0, distance_vector)
     accuracy = tf.keras.metrics.binary_accuracy(y_true, distance_vector)
     return accuracy
 
 
-@tf.function
 def simnet_loss(target, x1, x2):
     difference = x1 - x2
     distance_vector = tf.map_fn(lambda x: Encoder.distance(x), difference)
@@ -90,7 +80,6 @@ def simnet_loss(target, x1, x2):
     return average_loss
 
 
-@tf.function
 def train_step(images, labels):
     with tf.GradientTape() as tape:
         x1, x2 = images[:, 0, :, :, :], images[:, 1, :, :, :]
@@ -103,7 +92,6 @@ def train_step(images, labels):
     return loss
 
 
-@tf.function
 def test_step(x_test, y_test):
     out = model.make_predict(x_test, threshold=0.5)
     accuracy = tf.keras.metrics.binary_accuracy(out, y_test)
@@ -112,23 +100,21 @@ def test_step(x_test, y_test):
 
 if __name__ == '__main__':
 
-    n_total_pairs = 20000
+    n_total_pairs = 10000
     n_epoch = 10
     batch_size = 180
-    learning_rate = 0.02
+    learning_rate = 0.01
 
     n_pairs = int(np.sqrt(n_total_pairs / 2))
-    # print(f'Calling pair selector with n = {n_pairs}')
+    print(f'Calling pair selector with n = {n_pairs}')
     x_train, x_test, y_train, y_test = load_and_split(n_pairs=n_pairs, n_classes=2)
     x_train, x_test = shrink(x_train, x_test)
     y_train, y_test = y_train.astype('float32'), y_test.astype('float32')
     x_train, x_test = x_train.astype('float32'), x_test.astype('float32')
-    # print('Percentage of similar images', np.round(y_train.sum()/y_train.shape[0], 3))
+    print('Percentage of similar images', np.round(y_train.sum()/y_train.shape[0], 3))
 
     model = Encoder()
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    model.build(input_shape=(None, 28, 28, 1))
-    model.summary()
 
     for epoch in range(n_epoch):
         epoch_loss = 0
@@ -145,21 +131,16 @@ if __name__ == '__main__':
                  "val_accuracy:", val_accuracy, output_stream=sys.stdout)
 
     # plot embedding of encoded images
-    images = x_train.reshape((x_train.shape[0] * x_train.shape[1], 28, 28, 1))
-    embeddings_of_train = model.call_encoder(images)
-    if embeddings_of_train.shape[1] > 2:
-        print('Applying t-SNE')
-        embeddings_of_test = reduce_dim(np.array(embeddings_of_train))
-    plot_embedding(embeddings_of_train)
-    plt.show()
+    # images = x_train.reshape((x_train.shape[0] * x_train.shape[1], 28, 28, 1))
+    # embeddings_of_train = model(images)
+    # if embeddings_of_train.shape[1] > 2:
+    #     print('Applying t-SNE')
+    #     embeddings_of_test = reduce_dim(np.array(embeddings_of_train))
+    # plot_embedding(embeddings_of_train)
+    # plt.show()
 
-    ## saving weights
-    # model = ResNet34()
-    # model.build((1, 224, 224, 3))
-    # model.summary()
-    # model.save_weights('model_weights.h5')
-    #
-    # ## loading saved weights
-    # model_new = ResNet34()
-    # model_new.build((1, 224, 224, 3))
-    # model_new.load_weights('model_weights.h5')
+    # for i in range(x_train.shape[0]):
+    #     plot_pair(x_train[i], y_train[i])
+    #     plt.show()
+
+    # model.build(input_shape=(None, 28, 28, 1))
